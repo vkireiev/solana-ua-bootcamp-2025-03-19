4b1f734ffd19d3751f930f9d93c7880d049680a9
Viktor Kireiev <vkireiev.ua@gmail.com>
2025-04-11 22:59:43 +0300

    Task 2.8 - Rewrite on Rust Tasks 2.1 - 2.6

--- practice-2/task_2_8/src/main.rs
+++ practice-2/task_2_8/src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Task 2.8 (send_sol.rs)");
+    println!("Task 2.8 (send_sol.rs, create_token_mint.rs, create_token_account.rs, mint_tokens.rs, create_token_metadata.rs)");
 }
